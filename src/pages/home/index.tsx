b8e046777d11a06362413b5e013012ea000049e9
Teodora Joaquim <theodoraurosadacosta@gmail.com>
2025-03-10 19:55:31 +0000

    actualizzção do item

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,10 +2,15 @@ import { useEffect, useState } from "react";
 import TodoItem, { Task } from "./components/TodoItem";
 import { Container, List } from "@mui/material";
 import TodoForm from "./components/TodoForm";
+import { UpdateItem } from "./components/updateItem"; 
+import { CustomizedDialogs } from "../../components/modal"; 
+
 
 export default function Home() {
     const [todos, setTodos] = useState<Task[]>([]);
-     
+    const [open, setOpen] = useState(false); 
+    const [selectedTask, setSelectedTask] = useState<Task>();
+
     const removeItem = (id: string) => {
         const newTodos = todos.filter(item => item.id !== id);
         setTodos(newTodos)
@@ -15,17 +20,22 @@ export default function Home() {
         setTodos([...todos, todo]);
     };
     
+    const getTodoList = async ()=>{
+        const result = await (await fetch('http://localhost:3000/api/v1/tasks?limit=100',{method: "GET"})).json();
+        setTodos([])
+        setTodos(result.items) 
+    }
 
+    const handleUpdate = (id:string)=>{ 
+        const task = todos.filter((item)=> item.id === id);
+        setSelectedTask(task.pop())
+        setOpen(true)
+    }
   // FETCH GET Para consultar todos tarefas
     useEffect(() => {
         // GET request using fetch inside useEffect React hook
-        fetch('http://localhost:3000/api/v1/tasks?limit=100',{method: "GET"})
-            .then(response => response.json())
-            .then(data => {
-                console.log({data});
-                setTodos(data.items)
-            });
-
+        
+        (async ()=>{await getTodoList()})()
   
     }, []);
 
@@ -39,12 +49,23 @@ export default function Home() {
             <TodoForm todoHandler={addTaskToList} />
               <List sx={{ mt: 2 }}>
                 {todos.map((todo) => (
-                    <TodoItem updateItem={(id:string)=>{}} key={todo.id} item={todo} deleteItem={removeItem}  ></TodoItem>
+                    <TodoItem 
+                    updateItem={handleUpdate} 
+                    key={todo.id} item={todo} 
+                    deleteItem={removeItem}  
+                    ></TodoItem>
                     
                     
                 ))}
                 
-            </List>
+                </List>
+                <CustomizedDialogs open={open} handleClose={()=>{ setOpen(false)}} title="UPDATE TASK">
+                    {selectedTask && <UpdateItem updateList={async ()=>{
+                        setOpen(false)
+                        setTodos([])
+                        await getTodoList();
+                        }} item={selectedTask}></UpdateItem>}
+                </CustomizedDialogs>
         </Container>
     )
 }
